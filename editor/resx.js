8708ee0ff8017779669ac4beea3c653c80784439
Dominic Vonk <dominic@vonk.io>
2020-09-18 01:05:09 +0200

    Initial resx editor

---
+++ editor/resx.js
@@ -0,0 +1,103 @@
+// @ts-check
+
+// Script run within the webview itself.
+(function () {
+
+    // Get a reference to the VS Code webview api.
+    // We use this API to post messages back to our extension.
+
+    // @ts-ignore
+    const vscode = acquireVsCodeApi();
+
+
+    const notesContainer = /** @type {HTMLElement} */ (document.querySelector('tbody'));
+
+
+    const errorContainer = document.createElement('div');
+    document.body.appendChild(errorContainer);
+    errorContainer.className = 'error'
+    errorContainer.style.display = 'none'
+
+    /**
+     * Render the document in the webview.
+     */
+    function updateContent(/** @type {string} */ text) {
+        let json;
+        try {
+            json = JSON.parse(text);
+        } catch {
+            notesContainer.style.display = 'none';
+            errorContainer.innerText = 'Error: Document is not valid resx';
+            errorContainer.style.display = '';
+            return;
+        }
+        notesContainer.style.display = '';
+        errorContainer.style.display = 'none';
+        let inputEvent = () => {
+            let obj = {};
+            let a = notesContainer.querySelectorAll('tr');
+            for (let rule of a) {
+                let inputs = rule.querySelectorAll('input');
+                obj[inputs[0].value] = {
+                    value: inputs[1].value,
+                    comment: inputs[2].value
+                }
+            }
+            vscode.setState({ text: JSON.stringify(obj) });
+            vscode.postMessage({
+                type: 'update',
+                json: JSON.stringify(obj)
+            });
+        };
+        // Render the scratches
+        notesContainer.innerHTML = '';
+        for (const _name in json || []) {
+            let rule = json[_name];
+            const element = document.createElement('tr');
+            notesContainer.appendChild(element);
+
+            const name = document.createElement('td');
+            const __name = document.createElement('input');
+            __name.oninput = inputEvent;
+            name.appendChild(__name);
+            __name.value = _name;
+            const value = document.createElement('td');
+            const _value = document.createElement('input');
+            value.appendChild(_value);
+            _value.value = rule.value || '';
+            _value.oninput = inputEvent;
+            const comment = document.createElement('td');
+            const _comment = document.createElement('input');
+            comment.appendChild(_comment);
+            _comment.value = rule.comment || '';
+            _comment.oninput = inputEvent;
+            element.append(name, value, comment);
+        }
+
+    }
+
+    // Handle messages sent from the extension to the webview
+    window.addEventListener('message', event => {
+        const message = event.data; // The json data that the extension sent
+        switch (message.type) {
+            case 'update':
+                const text = message.text;
+                if (text != vscode.getState()?.text) {
+                    // Update our webview's content
+                    updateContent(text);
+                }
+                // Then persist state information.
+                // This state is returned in the call to `vscode.getState` below when a webview is reloaded.
+                vscode.setState({ text });
+
+                return;
+        }
+    });
+
+    // Webviews are normally torn down when not visible and re-created when they become visible again.
+    // State lets us save information across these re-loads
+    const state = vscode.getState();
+    if (state) {
+        updateContent(state.text);
+    }
+}());
---
+++ src/ResXEditorProvider.ts
@@ -0,0 +1,184 @@
+import * as path from 'path';
+import * as vscode from 'vscode';
+import { getNonce } from './util';
+import resx from 'resx';
+
+/**
+ * Provider for cat scratch editors.
+ * 
+ * Cat scratch editors are used for `.cscratch` files, which are just json files.
+ * To get started, run this extension and open an empty `.cscratch` file in VS Code.
+ * 
+ * This provider demonstrates:
+ * 
+ * - Setting up the initial webview for a custom editor.
+ * - Loading scripts and styles in a custom editor.
+ * - Synchronizing changes between a text document and a custom editor.
+ */
+export class ResXEditorProvider implements vscode.CustomTextEditorProvider {
+
+    public static register(context: vscode.ExtensionContext): vscode.Disposable {
+        const provider = new ResXEditorProvider(context);
+        const providerRegistration = vscode.window.registerCustomEditorProvider(ResXEditorProvider.viewType, provider);
+        return providerRegistration;
+    }
+
+    private static readonly viewType = 'resx.editor';
+
+    constructor(
+        private readonly context: vscode.ExtensionContext
+    ) { }
+
+    /**
+     * Called when our custom editor is opened.
+     * 
+     * 
+     */
+    public async resolveCustomTextEditor(
+        document: vscode.TextDocument,
+        webviewPanel: vscode.WebviewPanel,
+        _token: vscode.CancellationToken
+    ): Promise<void> {
+        // Setup initial content for the webview
+        webviewPanel.webview.options = {
+            enableScripts: true,
+        };
+        webviewPanel.webview.html = this.getHtmlForWebview(webviewPanel.webview);
+
+        async function updateWebview() {
+            webviewPanel.webview.postMessage({
+                type: 'update',
+                text: JSON.stringify(await resx.resx2js(document.getText(), true)),
+            });
+        }
+
+        // Hook up event handlers so that we can synchronize the webview with the text document.
+        //
+        // The text document acts as our model, so we have to sync change in the document to our
+        // editor and sync changes in the editor back to the document.
+        // 
+        // Remember that a single text document can also be shared between multiple custom
+        // editors (this happens for example when you split a custom editor)
+
+        const changeDocumentSubscription = vscode.workspace.onDidChangeTextDocument(e => {
+            if (e.document.uri.toString() === document.uri.toString()) {
+                updateWebview();
+            }
+        });
+
+        // Make sure we get rid of the listener when our editor is closed.
+        webviewPanel.onDidDispose(() => {
+            changeDocumentSubscription.dispose();
+        });
+
+        // Receive message from the webview.
+        webviewPanel.webview.onDidReceiveMessage(e => {
+            switch (e.type) {
+                case 'update':
+                    this.newChange(document, e.json);
+                    return;
+            }
+        });
+
+        updateWebview();
+    }
+
+    /**
+     * Get the static html used for the editor webviews.
+     */
+    private getHtmlForWebview(webview: vscode.Webview): string {
+        // Local path to script and css for the webview
+        const scriptUri = webview.asWebviewUri(vscode.Uri.file(
+            path.join(this.context.extensionPath, 'editor', 'resx.js')
+        ));
+        const styleUri = webview.asWebviewUri(vscode.Uri.file(
+            path.join(this.context.extensionPath, 'editor', 'resx.css')
+        ));
+
+        // Use a nonce to whitelist which scripts can be run
+        const nonce = getNonce();
+
+        return /* html */`
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+
+				<!--
+				Use a content security policy to only allow loading images from https or from our extension directory,
+				and only allow scripts that have a specific nonce.
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource}; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+				<link href="${styleUri}" rel="stylesheet" />
+
+				<title>ResX Viewer</title>
+			</head>
+			<body>
+                <table>
+                <thead>
+                    <th>Name</th>
+                    <th>Value</th>
+                    <th>Comment</th>
+                </thead>
+                <tbody>
+
+                </tbody>
+                </table>
+				
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
+    }
+
+    /**
+     * Add a new scratch to the current document.
+     */
+    private newChange(document: vscode.TextDocument, json: any) {
+        /* const json = this.getDocumentAsJson(document);
+         json.scratches = [
+             ...(Array.isArray(json.scratches) ? json.scratches : []),
+             {
+                 id: getNonce(),
+                 text: character,
+                 created: Date.now(),
+             }
+         ];
+ */
+        return this.updateTextDocument(document, json);
+    }
+
+    /**
+     * Try to get a current document as json text.
+     */
+    private getDocumentAsJson(document: vscode.TextDocument): any {
+        const text = document.getText();
+        if (text.trim().length === 0) {
+            return {};
+        }
+
+        try {
+            return JSON.parse(text);
+        } catch {
+            throw new Error('Could not get document as json. Content is not valid json');
+        }
+    }
+
+    /**
+     * Write out the json to a given document.
+     */
+    private async updateTextDocument(document: vscode.TextDocument, json: any) {
+        const edit = new vscode.WorkspaceEdit();
+
+        // Just replace the entire document every time for this example extension.
+        // A more complete extension should compute minimal edits instead.
+        edit.replace(
+            document.uri,
+            new vscode.Range(0, 0, document.lineCount, 0),
+            await resx.js2resx(JSON.parse(json)));
+
+        return vscode.workspace.applyEdit(edit);
+    }
+}
